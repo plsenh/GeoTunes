118bd7423cb21b34647e732df6f91657e14427e9
Evan Kozierachi <evan.kozierachi@gmail.com>
2018-11-01 20:53:14 -0400

    1st commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
-
     
+    $("#playlist-button").on("click", function (event) {
+        event.preventDefault();
+        var apiKeyLastFM = "8e58ab9ad2424bc14ac0944a801793cd";
+        var country = $("#country").val().trim();
+        var location = $("#city").val().trim();
+        var queryURL = "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&api_key=" + apiKeyLastFM + "&country=" + country + "&location=" + location + "&format=json";
+
+        $.ajax({
+
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            console.log(response);
+
+        })
+    });
+
 });
