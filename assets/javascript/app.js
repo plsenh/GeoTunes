e40d3ef0f97947f3d12f9d27e6917016815bc32c
Evan Kozierachi <evan.kozierachi@gmail.com>
2018-11-01 22:00:58 -0400

    working with Claudio

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     $("#playlist-button").on("click", function (event) {
         event.preventDefault();
         var apiKeyLastFM = "8e58ab9ad2424bc14ac0944a801793cd";
@@ -11,8 +11,21 @@ $(document).ready(function () {
 
             url: queryURL,
             method: "GET",
-        }).then(function (response) {
-            console.log(response);
+        }).then(function (tracks) {
+            const tracksResult = tracks.tracks;
+ 
+            const trackArray = [];
+            for (let i = 0; i < tracksResult.track.length; i++) {
+                console.log(tracksResult.track[i].artist.name);
+                console.log(tracksResult.track[i].name);
+                console.log('--------------------------------');
+                //an empty object and assign to a variable ;
+                // create a for loop to iterate through tracksResult.track[i] {
+                    //dynamically create key value pairs using square bracket notation and the index 
+                    //}
+                // push your new obj to trackArray
+
+            }
 
         })
     });
